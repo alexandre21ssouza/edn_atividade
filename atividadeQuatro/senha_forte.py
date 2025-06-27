045cdee3397036270238581dafc3e4d1c1547444
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2025-06-27 01:48:48 -0300

    Inserindo as informações adicionais aos exercícios

--- atividadeQuatro/senha_forte.py
+++ atividadeQuatro/senha_forte.py
@@ -1,3 +1,8 @@
+# Atividade 3
+
+# Crie um programa que verifique se uma senha é forte. Uma senha forte deve ter pelo menos 
+# 8 caracteres e conter pelo menos um número. O programa deve continuar pedindo senhas até 
+# que uma válida seja inserida ou o usuário digite 'sair'.
 
 
 notas = []
